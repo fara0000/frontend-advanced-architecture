ad81b2ab6e130e5d5001a1e132bb4040dd13d64e
fakhriimanzade <f.imanzade@newton-technology.ru>
2023-11-17 00:42:26 +0300

    Decomposition of Webpack configuration

---
+++ config/types/config.ts
@@ -0,0 +1,15 @@
+// config.ts
+
+export type BuildMode = 'development' | 'production';
+
+export interface BuildPaths {
+    entry: string;
+    build: string;
+    html: string;
+}
+
+export type BuildOptions = {
+    mode: BuildMode;
+    paths: BuildPaths;
+    isDev: boolean
+}
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,37 +1,20 @@
 import webpack from "webpack";
-import * as path from "path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import {buildWebpackConfig} from "./config/buildWebpackConfig";
+import path from "path";
+import {BuildPaths} from "./config/types/config";
 
-const config: webpack.Configuration = {
-    mode: "development",
+const mode = 'development';
+const paths: BuildPaths = {
     entry: path.resolve(__dirname, 'src', 'index.ts'),
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        // так же filename можно указывать динамически,
-        // тогда будет создаться в бандле main.js
-        // это помогает кэшировать файлы, браузер их кэширует
-        filename: "[name].[contenthash].js",
-        // очищает старые билды
-        clean: true
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'public', 'index.html')
-        }),
-        new webpack.ProgressPlugin(), //показывает прогресс
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/, // ts и tsx будет обрабатывать
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            }
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'], // расширение при импорте
-    }
+    build: path.resolve(__dirname, 'build'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
 }
+const isDev = mode === 'development';
+
+const config: webpack.Configuration = buildWebpackConfig({
+    mode,
+    paths,
+    isDev
+})
 
 export default config;
