e94901bf7aa3d46519e77fd4f850db63fcb2875e
fara0000 <fexri_imanzade@mail.ru>
2023-11-18 14:19:13 +0300

    Added React@17 and configured CSS, SCSS, SASS support

--- config/buildLoaders.ts
+++ config/buildLoaders.ts
@@ -1,13 +1,29 @@
 import webpack from 'webpack';
 
 export function buildLoaders(): webpack.RuleSetRule[] {
+
+    // Если не испольуем тайпскрипт то нужен babel-loader
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
     }
 
+    // Loader для стилей, !!! Порядок лоадеров в рамках стилей очень важен
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+        ],
+    }
+
     return [
         typescriptLoader,
+        cssLoader,
     ]
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -7,7 +7,7 @@ import {BuildPaths, EnvConfig} from "./config/types/config";
 export default (env: EnvConfig) => {
     const mode = env.mode || 'development';
     const paths: BuildPaths = {
-        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        entry: path.resolve(__dirname, 'src', 'index.tsx'),
         build: path.resolve(__dirname, 'build'),
         html: path.resolve(__dirname, 'public', 'index.html'),
     }
